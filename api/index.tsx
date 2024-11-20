01a5b22ee282b4ad5596f433e9974df7b988fad7
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-11-20 00:40:27 -0600

    hjgchjvjh

--- api/index.tsx
+++ api/index.tsx
@@ -407,6 +407,6 @@ function getCardDisplay(card: Card): { path: string; label: string } {
   const label = `${getCardLabel(card.v)} of ${suitMap[card.s]}`;
   return {
     label,
-    path: `/wargame/api/public/assets/cards/${card.v}_of_${suitMap[card.s]}.png`
+    path: `/public/${card.v}_of_${suitMap[card.s]}.png`
   };
 }
